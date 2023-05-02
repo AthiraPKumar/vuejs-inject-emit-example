c2670af79969be8a64bda2124e39e33ad432eaeb
AthiraPKumar <athirapkumar16@gmail.com>
2023-05-03 00:30:27 +0530

    this is a simple example of inject emit in vue

---
+++ src/components/KnowledgeElement.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: ['selectTopic'],
+  props: ['id', 'topicName', 'description'],
+  emits: ['select-topic'],
+};
+</script>
+
+<!-- use injector without passing props -->
+<!-- provide/inject substitute for input output, so that it lets components communicate -->
+<!-- only used for parent & child communication -->
+<!-- you should not always used provide/inject to replace custom/props -->
